243e9a0fc5e5ec8c021a3566ab66edee679be27a
DaeHyeonHong <fholng@outlook.com>
2022-04-03 21:51:58 +0900

    [JPA] Spring JPA Paging

--- src/main/java/study/jpa/controller/MemberController.java
+++ src/main/java/study/jpa/controller/MemberController.java
@@ -1,19 +1,24 @@
 package study.jpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.jpa.dto.MemberDto;
 import study.jpa.entity.Member;
 import study.jpa.repository.MemberRepository;
 import javax.annotation.PostConstruct;
+import java.util.stream.IntStream;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberRepository memberRepository;
 
-    @GetMapping("/members/{id}")
+    @GetMapping(value = "/members/{id}")
     public String findMember(@PathVariable final Long id) {
         return this.memberRepository.findById(id).orElseThrow().getUsername();
     }
@@ -23,8 +28,13 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping(value = "members")
+    public Page<MemberDto> list(@PageableDefault(size = 5, sort = "username") final Pageable pageable) {
+        return this.memberRepository.findAll(pageable).map(MemberDto::new);
+    }
+
     @PostConstruct
     public void init() {
-        this.memberRepository.save(new Member("userA"));
+        IntStream.range(0, 100).mapToObj(i -> new Member("user" + i, i)).forEach(this.memberRepository::save);
     }
 }
